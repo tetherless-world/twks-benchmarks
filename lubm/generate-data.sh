95fb464f45eae330633b8217f2bc5f41f442d7fb
Minor Gordon <gordom6@rpi.edu>
2019-11-25 10:17:52 -0500

    .gitignore data/, change generate-data.sh to write to it

--- lubm/generate-data.sh
+++ lubm/generate-data.sh
@@ -4,5 +4,8 @@ cd "$(dirname "$0")"
 
 mvn package
 
+mkdir -p data
+cd data
+
 # https://jena.apache.org/documentation/sdb/query_performance.html
-java -cp target/twks-benchmarks-lubm.jar edu.lehigh.swat.bench.uba.Generator -univ 1 -onto http://swat.cse.lehigh.edu/onto/univ-bench.owl
+java -cp ../target/twks-benchmarks-lubm.jar edu.lehigh.swat.bench.uba.Generator -univ 1 -onto http://swat.cse.lehigh.edu/onto/univ-bench.owl
