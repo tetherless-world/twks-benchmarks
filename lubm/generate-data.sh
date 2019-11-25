1db4548574519d84e79bc0b82d2a8e2e368e03e2
Minor Gordon <gordom6@rpi.edu>
2019-11-25 14:33:36 -0500

    switch back to only one University

--- lubm/generate-data.sh
+++ lubm/generate-data.sh
@@ -8,4 +8,5 @@ mkdir -p data
 cd data
 
 # https://jena.apache.org/documentation/sdb/query_performance.html
-java -cp ../target/twks-benchmarks-lubm.jar edu.lehigh.swat.bench.uba.Generator -univ 1 -onto http://swat.cse.lehigh.edu/onto/univ-bench.owl
+UNIV=1
+java -cp ../target/twks-benchmarks-lubm.jar edu.lehigh.swat.bench.uba.Generator -univ $UNIV -onto http://swat.cse.lehigh.edu/onto/univ-bench.owl
